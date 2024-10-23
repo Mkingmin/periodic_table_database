1817e0f3ae7e92b7b7d78026e52ba8bb01a417c9
Nguyen Vo Anh Minh <minhnguyen.31201021885@st.ueh.edu.vn>
2024-10-23 03:15:31 +0000

    feat: add commands to print atomic info

--- element.sh
+++ element.sh
@@ -10,4 +10,11 @@ CHECK=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE $1=atomic_
 if [[ -z $CHECK ]]
 then
 echo -e "\nI could not find that element in the database.\n"
+else
+ATOMIC_INFO=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE $1=atomic_number OR '$1'=symbol OR '$1'=name")
+  # Parse the result
+  echo "$ATOMIC_INFO" | while IFS="|" read TYPE_ID ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE
+  do
+    echo The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius.
+  done
 fi
