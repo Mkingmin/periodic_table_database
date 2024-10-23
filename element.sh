11c190b99430938977ee3dcdf41054386538ab03
Nguyen Vo Anh Minh <minhnguyen.31201021885@st.ueh.edu.vn>
2024-10-23 03:16:11 +0000

    fix: add newline

--- element.sh
+++ element.sh
@@ -15,6 +15,6 @@ ATOMIC_INFO=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_n
   # Parse the result
   echo "$ATOMIC_INFO" | while IFS="|" read TYPE_ID ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE
   do
-    echo The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius.
+    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius.\n"
   done
 fi
