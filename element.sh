2113be4dbe2fc3b72f5bd44989eadcaf91f793a0
Nguyen Vo Anh Minh <minhnguyen.31201021885@st.ueh.edu.vn>
2024-10-23 03:14:21 +0000

    feat: add check data input commands

--- element.sh
+++ element.sh
@@ -4,3 +4,10 @@ if [[ -z $1 ]]
 then
 echo -e "\nPlease provide an element as an argument.\n"
 fi
+# check if argument belongs to existing atomic
+CHECK=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE $1=atomic_number OR '$1'=symbol OR '$1'=name")
+# if it doesn't not belong to databse
+if [[ -z $CHECK ]]
+then
+echo -e "\nI could not find that element in the database.\n"
+fi
