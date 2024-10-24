020e1bccafaded9a070189436c8d084b7d242f72
Nguyen Vo Anh Minh <minhnguyen.31201021885@st.ueh.edu.vn>
2024-10-24 05:26:47 +0000

    fix: final fix

--- element.sh
+++ element.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
-if [[ $1 ]]
-  then
-  if [[ ! $1 =~ ^[0-9]+$ ]]
-  then
-  ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.name LIKE '$1%' ORDER BY atomic_number LIMIT 1")
-  else
-  ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.atomic_number=$1")
-  fi
-    if [[ -z $ELEMENT ]]
-    then
-      echo "I could not find that element in the database."
-    else
-      echo $ELEMENT | while IFS=\| read ATOMIC_NUMBER ATOMIC_MASS MPC BPC SY NAME TYPE
-      do
-        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SY). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MPC celsius and a boiling point of $BPC celsius." 
-      done
-      
-    fi
-  else
-  echo  "Please provide an element as an argument."
+if [[ -z $1 ]]
+then
+echo "Please provide an element as an argument."
+else
+# check if argument belongs to existing atomic
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  ATOMIC_INFO=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
+else
+  ATOMIC_INFO=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
+fi
+# if it doesn't not belong to databse
+if [[ -z $ATOMIC_INFO ]]
+then
+echo "I could not find that element in the database."
+else
+  # Parse the result
+  echo "$ATOMIC_INFO" | while IFS="|" read TYPE_ID ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
+fi
 fi
